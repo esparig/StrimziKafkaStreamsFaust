012280df16290109376a77ac46d4f48a564a04e3
esparig <5576714+esparig@users.noreply.github.com>
2024-03-01 12:17:13 +0100

    Fixed topic format and small mistake in line 23

--- faust-test.py
+++ faust-test.py
@@ -8,11 +8,11 @@ import faust
 #class ClientExample(faust.Record):
 #    log: str
 
-class ClientExample(faust.Record, serializer='raw'):
-    pass
+#class ClientExample(faust.Record, serializer='raw'):
+#    pass
 
 app = faust.App('myfaustapp', broker='kafka://my-cluster-kafka-bootstrap:9092')
-topic = app.topic('my-topic', value_type=ClientExample)
+topic = app.topic('my-topic', value_type=bytes)
 
 @app.agent(topic)
 async def process_logs(logs):
@@ -20,7 +20,7 @@ async def process_logs(logs):
         print(f"Received message: {log.decode('utf-8')}")
         # Extract the number from the log message
         try:
-            number = int(log.decode('utf-8').message.split()[-1])
+            number = int(log.decode('utf-8').split()[-1])
             print(f"Extracted number: {number}")
         except ValueError:
             # Ignore non-numeric messages
